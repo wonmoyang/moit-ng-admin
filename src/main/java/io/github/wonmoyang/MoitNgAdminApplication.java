0fbf230cb87d0bd2ab00e9432355f9aa6ed80df7
wmyang <yangwm89@gmail.com>
2019-01-02 11:51:51 +0900

    first commit. (boot)

---
+++ src/main/java/io/github/wonmoyang/MoitNgAdminApplication.java
@@ -0,0 +1,14 @@
+package io.github.wonmoyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoitNgAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoitNgAdminApplication.class, args);
+	}
+
+}
+
